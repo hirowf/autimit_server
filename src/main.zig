420e195f52fa82b846a6424c5b0830476587b1b1
hirowf <hiroto.anderson@gmail.com>
2025-05-07 03:05:30 -0300

    connection pool with zpg

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,37 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("autimit_lib");
+const zpg = @import("zpg");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = &gpa.allocator;
+    defer gpa.deinit();
+
+    const cfg = zpg.Config{
+        .host = "127.0.0.1",
+        .port = 5432,
+        .username = "user",
+        .database = "autimit_db",
+        .password = "pass",
+        .tls_mode = .disable,
+        .timeout = 10_000,
+    };
+    try cfg.validate();
+
+    var pool = try zpg.ConnectionPool.init(allocator, cfg, 5);
+    defer pool.deinit();
+
+    var pconn = try zpg.PooledConnection.init(&pool, null);
+    defer pconn.deinit();
+
+    var query = pconn.createQuery(allocator);
+    defer query.deinit();
+
+    const sql = "SELECT now()";
+    const result = try query.run(sql, struct { now: []const u8 });
+    defer allocator.free(result);
+
+    if (result.len > 0) {
+        std.debug.print("DB time: {s}\n", .{result[0].now});
+    }
+}
